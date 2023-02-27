0ed0ff54be0c28971dd2d8dd4548b225e8f80bdf
heyimcsy <cinnamon1995@naver.com>
2023-02-27 17:56:19 +0900

    입력값 뜨도록 도출

--- src/App.js
+++ src/App.js
@@ -1,9 +1,53 @@
-import './App.css';
+import React,{ useState } from "react";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [doing, setDoing] = useState ([
+    { id: 1, bigTd:'리액트를 공부하자', smallTd:'리액트 노션 보기!'},
+    { id: 2, bigTd:'리액트를 공부하자', smallTd:'리액트 노션 보자!'},
+    { id: 3, bigTd:'리액트를 공부하자', smallTd:'리액트 노션 보고!'},
+    { id: 4, bigTd:'리액트를 공부하자', smallTd:'리액트 노션 보자기!'},
+  ]);
+  const [bigTd, setBigTd] = useState('');
+  const [smallTd, setSmallTd] = useState('');
+
+  const changeBidTdHandler = (event) => {
+    setBigTd(event.target.value);
+  }
+  const changeSmallTdHandler = (event) => {
+    setSmallTd(event.target.value);
+  }
+
   return (
     <div className="App">
-      HELLO
+      <div className="headStyle">
+      <p>My Todo List</p> 
+      <p>React</p>
+      </div>
+      <div>
+        제목 &nbsp;
+        <input 
+          value = {bigTd}
+          onChange = {changeBidTdHandler}/>
+          {bigTd}
+        내용 &nbsp;
+        <input 
+          value = {smallTd}
+          onChange = {changeSmallTdHandler}/>
+          {smallTd}
+        <button>추가하기</button>
+      </div>
+      <h1>Working..</h1>
+      <div className="toDoBox">
+        {doing.map((item) => {
+          return (
+          <div key={item.id} className="box-style">
+          <p>{item.bigTd}</p>
+          <p>{item.smallTd}</p>
+        </div> 
+          );
+        })}
+      </div>
     </div>
   );
 }
